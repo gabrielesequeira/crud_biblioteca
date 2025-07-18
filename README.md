7fce7bf6d39587a3f694f1a03ea66e9ed9be23b8
Gabriele <gabrielefs2005@gmail.com>
2025-07-18 16:23:56 -0300

    finished

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Projeto da disciplina **CTC4002 - Modelagem e Programação**  
 Feito com **Python** utilizando arquivos **JSON** para persistência de dados.
 
----
 
 ## Objetivo
 
@@ -18,6 +17,18 @@ O projeto simula um sistema de biblioteca, permitindo registrar livros com títu
 - [x] Atualizar informações de um livro
 - [x] Remover um livro do sistema
 - [x] Persistência de dados com JSON
+
+## Lógica de dados do Backend
+O sistema utiliza duas funções principais para gerenciar os dados dos livros:
+
+`carregar_dados()`
+Verifica se o arquivo JSON com os livros existe, lê e converte o conteúdo para um dicionário Python com a lista de livros. Se o arquivo não existir ou estiver vazio, retorna uma lista vazia para evitar erros.
+
+`salvar_dados(dados)`
+Recebe os dados atualizados dos livros como `dados` e grava no arquivo JSON, garantindo que todas as alterações sejam persistidas. Isso mantém o sistema sincronizado e com as informações sempre atualizadas entre as execuções.
+
+Essas funções garantem que o programa possa ler, modificar e salvar os dados de forma segura e eficiente, formando a base para as operações de cadastro, consulta, atualização e remoção (CRUD).
+
 
 ## Estrutura de dados
 
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import json  # read and write .json files
 import os    # create file paths that work on Windows
+import unicodedata
+
+#function from library
+def remover_acentos(texto):
+    texto_normalizado = unicodedata.normalize('NFD', texto)
+    texto_sem_acentos = texto_normalizado.encode('ascii', 'ignore').decode('utf-8')
+    return texto_sem_acentos
 
 CAMINHO_ARQUIVO = os.path.join("data", "livros.json")
 
@@ -63,10 +70,42 @@ def listar_livros():
         print("-" * 40)
 
 def buscar_livro():
-    pass
+    dados = carregar_dados()
+    livros = dados["livros"]
+    buscado = input("Qual nome do livro você quer buscar? ")
+
+    for livro in livros :
+        if remover_acentos(livro["titulo"]).lower() == buscado.lower():
+            print("\n==> Livro encontrado \n")
+            for chave,valor in livro.items():
+                print(f"{chave.capitalize()} : {valor}")
+            return
+        
+    print("\n:( Livro não encontrado")
+
+    
 
 def atualizar_livro():
-    pass
+    dados = carregar_dados()
+    livros = dados["livros"]
+
+    mudanca = input("Qual nome do livro deseja atualizar dados?")
+    for livro in livros:
+        if remover_acentos(livro["titulo"]).lower() == mudanca.lower():
+            tipo = input("Qual dos parametros deseja mudar (titulo / autor/ ano / editora) ? ")
+            if tipo in ["titulo", "autor", "ano", "editora"]:
+                novo_valor = input(f"Digite o novo valor para {tipo}: ")
+                livro[tipo] = novo_valor
+                salvar_dados(dados)
+                print("Valores atualizados com sucesso!")
+                for chave,valor in livro.items():
+                    print(f"{chave.capitalize()} : {valor}")
+                return
+            print(":( Parametro inválido")
+            return
+    print(":( Livro não encontrado")
+
+            
 
 def remover_livro():
     dados = carregar_dados()
@@ -74,23 +113,21 @@ def remover_livro():
 
     titulo = input("Digite o título do livro que deseja remover: ").strip()
 
-    encontrado = False
     for livro in livros:
-        if livro["titulo"].lower() == titulo.lower():
+        if remover_acentos(livro["titulo"]).lower() == titulo.lower():
             livros.remove(livro)
             encontrado = True
             print(f"\nLivro '{titulo}' removido com sucesso.")
-            break
+            salvar_dados(dados)
+            return
+        
+    print(":( Livro não encontrado)")
 
-    if not encontrado:
-        print(f"\nO Livro '{titulo}' não foi encontrado.")
-
-    salvar_dados(dados)
+    
 
 def sair():
-    salvar_dados(dados)
-    print("Saindo...")
-
+    print("\nSalvando dados....")
+    print("Saindo....")
 
 
 
@@ -103,7 +140,7 @@ opcoes = {
     "3": buscar_livro,
     "4": atualizar_livro,
     "5": remover_livro,
-    "6": sair 
+    "6": sair
 }
 
 while True:
